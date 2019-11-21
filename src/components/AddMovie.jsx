f69be840eb210ac9a472f2bf67a1b8056b928982
Richard Wellerson <richardwellerson@gmail.com>
2019-11-21 17:45:43 -0300

    Feita a inserçao das labels no AddMovie. Agora, fazer o select e terminar o componente.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action'
+    }
+  }
+  change = (event) => {
+    const { value, id } = event.target;
+    this.setState({
+      [id]: value
+    })
+  }
+
   render() {
+    const { onClick } = this.props.movies;
+    return (
+      <form>
+        <label>Título
+            <input type="text" id="title" value={this.state.title} onChange={this.setState} />
+        </label>
+        <label>Subtitulo
+            <input type="text" id="subtitle" value={this.state.subtitle} onChange={this.setState} />
+        </label>
+        <label>Imagem
+            <input type="text" id="imagePath" value={this.state.imagePath} onChange={this.setState} />
+        </label>
+        <label>Sinopse
+            <textarea id="storyline" value={this.state.imagePath} onChange={this.setState} />
+        </label>
+        <label>Avaliação
+            <input type="number" id="rating" value={this.state.rating} onChange={this.setState} />
+        </label>
+      </form>
+    )
   }
 }
 
