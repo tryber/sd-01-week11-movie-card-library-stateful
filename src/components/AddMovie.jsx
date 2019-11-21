68fdf01455671d7d1965a7ce5e064838f0e1cf2d
Richard Wellerson <richardwellerson@gmail.com>
2019-11-21 18:57:27 -0300

    Problema com AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,6 +12,7 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action'
     }
+
   }
   change = (event) => {
     const { value, id } = event.target;
@@ -20,25 +21,34 @@ class AddMovie extends React.Component {
     })
   }
 
+
   render() {
     const { onClick } = this.props.movies;
     return (
       <form>
         <label>Título
-            <input type="text" id="title" value={this.state.title} onChange={this.setState} />
+            <input type="text" id="title" value={this.state.title} onChange={this.change} />
         </label>
         <label>Subtitulo
-            <input type="text" id="subtitle" value={this.state.subtitle} onChange={this.setState} />
+            <input type="text" id="subtitle" value={this.state.subtitle} onChange={this.change} />
         </label>
         <label>Imagem
-            <input type="text" id="imagePath" value={this.state.imagePath} onChange={this.setState} />
+            <input type="text" id="imagePath" value={this.state.imagePath} onChange={this.change} />
         </label>
         <label>Sinopse
-            <textarea id="storyline" value={this.state.imagePath} onChange={this.setState} />
+            <textarea id="storyline" value={this.state.imagePath} onChange={this.change} />
         </label>
         <label>Avaliação
-            <input type="number" id="rating" value={this.state.rating} onChange={this.setState} />
+            <input type="number" id="rating" value={this.state.rating} onChange={this.change} />
         </label>
+        <label>Gênero
+          <select id="genre" value={this.state.genre} onChange={this.change}>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select>
+        </label>
+        <button type="button">Adicionar Filme</button>
       </form>
     )
   }
