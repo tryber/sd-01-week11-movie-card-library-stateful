ee02fdeb882009457fe38cb9d4e1d917c1bcf95b
Guilherme Luiz <guiiluiz44@gmail.com>
2019-11-22 15:54:50 -0300

    cria componente MovieLibrary e chama SearchBar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -7,13 +7,33 @@ import AddMovie from './AddMovie';
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+  }
+
+  changeHandler = event => {
+    const { name, value } = event.target
+    console.log(name,value)
+    this.setState(() => ({
+      [name]: value
+    }))
   }
 
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
+        <SearchBar
+          searchText={this.state.searchText}
+          onSearchTextChange={this.changeHandler}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          onBookmarkedChang={this.changeHandler}
+          selectedGenre={this.state.selectedGenre}
+          onSelectedGenreChange={this.changeHandler} />
         <MovieList movies={this.props.movies} />
         <AddMovie />
       </div>
