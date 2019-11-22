48e90c1422ff5540b6722c7925da3af592923b7e
Henrique Eyer <henriqueeyer@outlook.com>
2019-11-22 17:15:37 -0300

    code climate

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -16,7 +16,7 @@ class AddMovie extends React.Component {
 
   changeHandler = (event, name) => {
     let { value } = event.target
-    if(name==='rating'){value = Number(value)};
+    if (name === 'rating') { value = Number(value) };
     this.setState(() => ({
       [name]: value
     }));
@@ -38,27 +38,27 @@ class AddMovie extends React.Component {
   render() {
     const { onClick } = this.props;
     return (
-        <form>
-          <fieldset>
-            <label>Título<input type="text" value={this.state.title} onChange={(e) => this.changeHandler(e, 'title')} /></label>
-            <label>Subtítulo
+      <form>
+        <fieldset>
+          <label htmlFor="">Título<input type="text" value={this.state.title} onChange={(e) => this.changeHandler(e, 'title')} /></label>
+          <label htmlFor="">Subtítulo
             <input type="text" value={this.state.subtitle} onChange={(e) => this.changeHandler(e, 'subtitle')} /></label>
-            <label>Imagem
+          <label htmlFor="">Imagem
             <input type="text" value={this.state.imagePath} onChange={(e) => this.changeHandler(e, 'imagePath')} /></label>
-            <label>Sinopse
-            <textarea value={this.state.storyline} onChange={(e) => 
-            this.changeHandler(e, 'storyline')}/></label>
-            <label>Avaliação
+          <label htmlFor="">Sinopse
+            <textarea value={this.state.storyline} onChange={(e) =>
+              this.changeHandler(e, 'storyline')} /></label>
+          <label htmlFor="">Avaliação
             <input type="number" value={this.state.rating} onChange={(e) => this.changeHandler(e, 'rating')} /></label>
-            <label>Gênero
+          <label htmlFor="">Gênero
             <select onChange={(e) => this.changeHandler(e, 'genre')} value={this.state.genre} >
-                <option value="action">Ação</option>
-                <option value="comedy">Comédia</option>
-                <option value="thriller">Suspense</option>
-              </select></label>
-          </fieldset>
-          <button type="button" onClick={() => this.btnSaveMovie(onClick)}>Adicionar filme</button>
-        </form>
+              <option value="action">Ação</option>
+              <option value="comedy">Comédia</option>
+              <option value="thriller">Suspense</option>
+            </select></label>
+        </fieldset>
+        <button type="button" onClick={() => this.btnSaveMovie(onClick)}>Adicionar filme</button>
+      </form>
     );
   }
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,21 +3,21 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   render() {
     const {
-      searchText, onSearchTextChange,bookmarkedOnly,
-      onBookmarkedChange,selectedGenre, onSelectedGenreChange,
+      searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange,
     } = this.props;
     return (
-      <form> <fieldset><label> Inclui o texto:
+      <form> <fieldset><label htmlFor=""> Inclui o texto:
             <input type="text" value={searchText} onChange={onSearchTextChange} /></label>
-          <label>Mostrar somente favoritos:
+        <label htmlFor="">Mostrar somente favoritos:
             <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} /></label>
-          <label>Filtrar por gênero
+        <label htmlFor="">Filtrar por gênero
             <select onChange={onSelectedGenreChange} value={selectedGenre}>
-              <option value="">Todos</option>
-              <option value="action">Ação</option>
-              <option value="comedy">Comédia</option>
-              <option value="thriller">Suspense</option>
-            </select> </label> </fieldset> </form>
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select> </label> </fieldset> </form>
     );
   }
 }
