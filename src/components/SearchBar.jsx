d67afe496bd03125f88100ca8d2c18d0d78932eb
Henrique Eyer <henriqueeyer@outlook.com>
2019-11-22 16:44:03 -0300

    Terminado os testes, code climate

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,36 +2,22 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
+    const { 
+      searchText, onSearchTextChange,bookmarkedOnly,
+      onBookmarkedChange,selectedGenre, onSelectedGenreChange,
     } = this.props;
     return (
-      <form>
-        <fieldset>
-          <label>
-            Inclui o texto:
-            <input type="text" value={searchText} onChange={onSearchTextChange} />
-          </label>
-          <label>
-            Mostrar somente favoritos:
-            <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
-          </label>
-          <label>
-            Filtrar por gênero
+      <form> <fieldset><label> Inclui o texto:
+            <input type="text" value={searchText} onChange={onSearchTextChange} /></label>
+          <label>Mostrar somente favoritos:
+            <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} /></label>
+          <label>Filtrar por gênero
             <select onChange={onSelectedGenreChange} value={selectedGenre}>
-              <option value=''>Todos</option>
+              <option value="">Todos</option>
               <option value="action">Ação</option>
               <option value="comedy">Comédia</option>
               <option value="thriller">Suspense</option>
-            </select>
-          </label>
-        </fieldset>
-      </form>
+            </select> </label> </fieldset> </form>
     );
   }
 }
