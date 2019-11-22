c21b7d1784d1d58478c103fd1f6275110fd6bff3
Henrique Eyer <henriqueeyer@outlook.com>
2019-11-22 17:47:17 -0300

    AAAA

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,20 +7,21 @@ class SearchBar extends Component {
       onBookmarkedChange, selectedGenre, onSelectedGenreChange,
     } = this.props;
     return (
-      <form> <fieldset><label htmlFor="title"> Inclui o texto:
-            <input id="title" type="text" value={searchText} 
-            onChange={onSearchTextChange} /></label>
+      <form> <fieldset><label htmlFor="title">Inclui o texto:
+            <input id="title" type="text" value={searchText} onChange={onSearchTextChange}
+        /></label>
         <label htmlFor="favorited">Mostrar somente favoritos:
-            <input id="favorited" type="checkbox" checked={bookmarkedOnly} 
+            <input id="favorited" type="checkbox" checked={bookmarkedOnly}
             onChange={onBookmarkedChange} /></label>
         <label htmlFor="genre">Filtrar por gênero
-            <select id="genre" onChange={onSelectedGenreChange} 
+            <select id="genre" onChange={onSelectedGenreChange}
             value={selectedGenre}>
             <option value="">Todos</option>
             <option value="action">Ação</option>
             <option value="comedy">Comédia</option>
             <option value="thriller">Suspense</option>
-          </select> </label> </fieldset> </form>
+          </select> </label> </fieldset>
+      </form>
     );
   }
 }
