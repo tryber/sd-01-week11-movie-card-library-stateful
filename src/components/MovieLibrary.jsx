aa6e9c771193edf2181c4e5e9021769b99e8e2a7
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2019-11-22 19:51:53 -0300

    Avançando no MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -7,13 +7,40 @@ import AddMovie from './AddMovie';
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre:'',
+      movies: props.movies
+    }
   }
+
+  changeHandler = (event, name) => {
+    const { value } = event.target;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  changeHandlerCheckbox(event) {
+    this.setState(() => ({
+      bookmarkedOnly: event.target.checked
+    }))
+  }
+  
   render() {
     const { movies } = this.props
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
+        <SearchBar
+        searchText={this.state.searchText}
+        onSearchTextChange={event => this.changeHandler(event, 'searchText')}
+        bookmarkedOnly={this.state.bookmarkedOnly}
+        onBookmarkedChange={(event) => this.changeHandlerCheckbox(event)}
+        selectedGenre={this.state.selectedGenre}
+        onSelectedGenreChange={event => this.changeHandler(event, 'selectedGenre')}
+        />
         <MovieList movies={this.props.movies} />
         <AddMovie /> 
       </div>
