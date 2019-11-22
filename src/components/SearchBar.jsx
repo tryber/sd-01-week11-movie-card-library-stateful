795a8aedebaec20d20c940ae5748b68a2dd818b3
Victor Felipe <vf.ramos7@gmail.com>
2019-11-22 10:27:56 -0300

    Consertado alguns erros do CodeClimate

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,30 +2,24 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
-    } = this.props;
+    const { searchText,onSearchTextChange,bookmarkedOnly,
+      onBookmarkedChange,selectedGenre, onSelectedGenreChange} = this.props;
 
     return (
       <form>
         <div>
-          <label>Inclui o texto:</label>
-          <input type="text" value={searchText} onChange={onSearchTextChange}></input>
+          <label htmlFor="searchText">Inclui o texto:</label>
+          <input id="searchText" type="text" value={searchText} onChange={onSearchTextChange}></input>
         </div>
 
         <div>
-          <label>Mostrar somente favoritos</label>
-          <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+          <label htmlFor="checkbox">Mostrar somente favoritos</label>
+          <input id="checkbox" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
         </div>
 
         <div>
-          <label>Filtrar por gênero</label>
-          <select value={selectedGenre} onChange={onSelectedGenreChange}>
+          <label htmlFor="selectedGenre">Filtrar por gênero</label>
+          <select id="selectedGenre" value={selectedGenre} onChange={onSelectedGenreChange}>
             <option value="">Todos</option>
             <option value="action">Ação</option>
             <option value="comedy">Comédia</option>
@@ -34,6 +28,6 @@ class SearchBar extends Component {
         </div>
       </form>
     );
-  };
+  }
 }
 export default SearchBar;
