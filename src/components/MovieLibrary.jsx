de70e606fd7c8adf440eea596656ae8e6070fba5
Richard Wellerson <richardwellerson@gmail.com>
2019-11-22 18:51:36 -0300

    Terminada o filtro de adiçao de todos os campos. Agora, trabalhar para inserir os filmes no final.

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
+import MovieList from './MovieList';
 import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  render() {
-    return <h2>My awesome Movie Library</h2>;
-    <SearchBar />
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies
+    }
+  }
+
+  sendNewData = (event, id) => {
+    const { value } = event.target;
+    this.setState({
+      [id]: value
+    })
+  }
+
+  sendValueCheckbox = (event) => {
+    this.setState({
+      bookmarkedOnly: event.target.checked
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar
+          searchText={this.state.searchText}
+          onSearchTextChange={(e) => this.sendNewData(e, 'searchText')}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          onBookmarkedChange={(e) => this.sendValueCheckbox(e)}
+          selectedGenre={this.state.selectedGenre}
+          onSelectedGenreChange={(e) => this.sendNewData(e, 'selectedGenre')}
+        />
+        <MovieList movies={this.props.movies} />
+        <AddMovie />
+      </div>
+    );
   }
 }
 
