3c14e4d9646ea22769c3ecfaf805295852e4b141
Victor Felipe <vf.ramos7@gmail.com>
2019-11-24 14:33:59 -0300

    CodeClimate 1.6

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,12 +5,13 @@ class SearchBar extends Component {
     return (
       <label htmlFor="searchText">
         Inclui o texto:
-      <input
-          name="searchText"
-          type="text"
-          value={this.props.searchText}
-          onChange={this.props.onSearchTextChange}
-        />
+          <input
+            id="searchText"
+            name="searchText"
+            type="text"
+            value={this.props.searchText}
+            onChange={this.props.onSearchTextChange} 
+          />
       </label>
     );
   }
@@ -19,7 +20,7 @@ class SearchBar extends Component {
     return (
       <label htmlFor="checkbox">
         Mostrar somente favoritos
-      <input name="checkbox"
+        <input name="checkbox"
           type="checkbox"
           checked={this.props.bookmarkedOnly}
           onChange={this.props.onBookmarkedChange}
@@ -32,7 +33,7 @@ class SearchBar extends Component {
     return (
       <label htmlFor="selectedGenre">
         Filtrar por gênero
-        <select name="selectedGenre"
+          <select name="selectedGenre"
           value={this.props.selectedGenre}
           onChange={this.props.onSelectedGenreChange}>
           <option value="">Todos</option>
