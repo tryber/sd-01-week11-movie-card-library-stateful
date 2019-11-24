77a4657ec2883c3ee0b1cf238aad40441c7e347d
Richard Wellerson <richardwellerson@gmail.com>
2019-11-24 13:18:38 -0300

    Refatorando destructuring para o CC

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly, 
-      onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange
+    } = this.props;
       
     return (
       <form>
