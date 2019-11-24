3853748c55c05cfb02d57ed25bbbcaa3878e100d
Victor Felipe <vf.ramos7@gmail.com>
2019-11-24 13:10:29 -0300

    Refatorando o Search Bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,34 +1,44 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  render() {
-    const { 
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre, 
-      onSelectedGenreChange} = this.props;
+  renderInputText() {
+    return (
+      <div>
+        <label htmlFor="searchText">Inclui o texto:</label>
+        <input name="searchText" type="text" value={this.props.searchText} onChange={this.props.onSearchTextChange}></input>
+      </div>
+    );
+  }
 
+  renderBookmarkedCheckbox() {
+    return (
+      <div>
+        <label htmlFor="checkbox">Mostrar somente favoritos</label>
+        <input name="checkbox" type="checkbox" checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange} />
+      </div>
+    );
+  }
+
+  renderGenreSelector() {
+    return (
+      <div>
+        <label htmlFor="selectedGenre">Filtrar por gênero</label>
+        <select name="selectedGenre" value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+      </div>
+    );
+  }
+
+  render() {
     return (
       <form>
-        <div>
-          <label htmlFor="searchText">Inclui o texto:</label>
-          <input name="searchText" type="text" value={searchText} onChange={onSearchTextChange}></input>
-        </div>
-        <div>
-          <label htmlFor="checkbox">Mostrar somente favoritos</label>
-          <input name="checkbox" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
-        </div>
-        <div>
-          <label htmlFor="selectedGenre">Filtrar por gênero</label>
-          <select name="selectedGenre" value={selectedGenre} onChange={onSelectedGenreChange}>
-            <option value="">Todos</option>
-            <option value="action">Ação</option>
-            <option value="comedy">Comédia</option>
-            <option value="thriller">Suspense</option>
-          </select>
-        </div>
+        {this.renderInputText()}
+        {this.renderBookmarkedCheckbox()}
+        {this.renderGenreSelector()}
       </form>
     );
   }
