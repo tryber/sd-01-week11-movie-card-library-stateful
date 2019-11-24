adfad9fefc546447f8c5f342ae0687c08620e13d
Anderson Bolivar <bolivar.anderson@hotmail.com>
2019-11-24 20:08:52 -0200

    Fix Error Test

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -13,9 +13,34 @@ class AddMovie extends React.Component {
     }
   }
 
-  onTextChange = event => {
-    const { name, value } = event.target
-    this.setState({ [name]: value })
+  onTitleChange = event => {
+    const { value } = event.target
+    this.setState({ title: value })
+  }
+
+  onSubChange = event => {
+    const { value } = event.target
+    this.setState({ subtitle: value })
+  }
+
+  onImageChange = event => {
+    const { value } = event.target
+    this.setState({ imagePath: value })
+  }
+
+  onStoryChange = event => {
+    const { value } = event.target
+    this.setState({ storyline: value })
+  }
+
+  onRatingChange = event => {
+    const { value } = event.target
+    this.setState({ rating: Number(value) })
+  }
+
+  onGenreChange = event => {
+    const { value } = event.target
+    this.setState({ genre: value })
   }
 
   clickHere = () => {
@@ -40,27 +65,27 @@ class AddMovie extends React.Component {
       <form>
         <label htmlFor="film-title">
           Título
-        <input type="text" id="film-title" name="title" value={this.state.title} onChange={this.onTextChange} />
+        <input type="text" id="film-title" name="title" value={this.state.title} onChange={this.onTitleChange} />
         </label>
         <label htmlFor="film-subtitle">
           Subtítulo
-        <input type="text" id="film-subtitle" name="subtitle" value={this.state.subtitle} onChange={this.onTextChange} />
+        <input type="text" id="film-subtitle" name="subtitle" value={this.state.subtitle} onChange={this.onSubChange} />
         </label>
         <label htmlFor="film-img">
           Imagem
-        <input type="text" id="film-img" name="imagePath" value={this.state.imagePath} onChange={this.onTextChange} />
+        <input type="text" id="film-img" name="imagePath" value={this.state.imagePath} onChange={this.onImageChange} />
         </label>
         <label htmlFor="film-storyline">
           Sinopse
-        <textarea id="film-storyline" name="storyline" value={this.state.storyline} onChange={this.onTextChange} />
+        <textarea id="film-storyline" name="storyline" value={this.state.storyline} onChange={this.onStoryChange} />
         </label>
         <label htmlFor="film-avalation">
           Avaliação
-        <input type="number" id="film-avalation" name="rating" value={this.state.rating} onChange={this.onTextChange} />
+        <input type="number" id="film-avalation" name="rating" value={this.state.rating} onChange={this.onRatingChange} />
         </label>
         <label htmlFor="film-genre">
           Gênero
-        <select name="genre" value={this.state.genre} onChange={this.onTextChange}>
+        <select name="genre" value={this.state.genre} onChange={this.onGenreChange}>
             {genreOptions.map((option, index) => <option key={index} value={option.value}>{option.text}</option>)}
           </select>
         </label>
