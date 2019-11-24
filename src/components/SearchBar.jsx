9f2d97a85a989bb888fedd75cb3e42cb5d761ec6
Henrique Eyer <henriqueeyer@outlook.com>
2019-11-24 10:32:23 -0300

    Refatorando searchbar para o render ficar com menos de 25 linhas

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,41 +1,59 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class SearchBar extends Component {
-  render() {
-    const {
-      searchText, onSearchTextChange,
-      bookmarkedOnly, onBookmarkedChange,
-      selectedGenre, onSelectedGenreChange,
-    } = this.props;
+function SearchBar(props) {
+  const {
+    searchText, onSearchTextChange,
+    bookmarkedOnly, onBookmarkedChange,
+    selectedGenre, onSelectedGenreChange,
+  } = props;
+
+  const createInput = () => {
     return (
-      <form>
-        <fieldset>
-          <label htmlFor="title">
-            Inclui o texto:
-            <input id="title" type="text" value={searchText} onChange={onSearchTextChange} />
-          </label>
-          <label htmlFor="favorited">
-          Mostrar somente favoritos:
-            <input
-              id="favorited"
-              type="checkbox"
-              checked={bookmarkedOnly}
-              onChange={onBookmarkedChange}
-            />
-          </label>
-          <label htmlFor="genre">
-            Filtrar por gênero
-            <select id="genre" onChange={onSelectedGenreChange} value={selectedGenre}>
-              <option value="">Todos</option>
-              <option value="action">Ação</option>
-              <option value="comedy">Comédia</option>
-              <option value="thriller">Suspense</option>
-            </select>
-          </label>
-        </fieldset>
-      </form>
+      <label htmlFor="title">
+      Inclui o texto:
+        <input id="title" type="text" value={searchText} onChange={onSearchTextChange} />
+      </label>
     );
-  }
+  };
+
+  const createInputChecked = () => {
+    return (
+      <label htmlFor="favorited">
+      Mostrar somente favoritos:
+        <input
+          id="favorited"
+          type="checkbox"
+          checked={bookmarkedOnly}
+          onChange={onBookmarkedChange}
+        />
+      </label>
+    );
+  };
+
+  const createSelect = () => {
+    return (
+      <label htmlFor="genre">
+      Filtrar por gênero
+        <select id="genre" onChange={onSelectedGenreChange} value={selectedGenre}>
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  };
+
+  return (
+    <form>
+      <fieldset>
+        {createInput()}
+        {createInputChecked()}
+        {createSelect()}
+      </fieldset>
+    </form>
+  );
 }
+
 
 export default SearchBar;
