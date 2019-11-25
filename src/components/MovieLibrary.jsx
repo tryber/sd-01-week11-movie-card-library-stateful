12c83a56a7d878413c634d0129f2a53190691a66
Richard Wellerson <richardwellerson@gmail.com>
2019-11-25 13:36:52 -0300

    Correçoes CodeClimate

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -14,28 +14,30 @@ class MovieLibrary extends Component {
       movies: props.movies,
     };
 
-    this.filterFilms = this.filterFilms.bind(this)
-    this.sendNewData = this.sendNewData.bind(this)
-    this.sendValueCheckbox = this.sendValueCheckbox.bind(this)
-    this.addFilmToList =this.addFilmToList.bind(this)
+    this.filterFilms = this.filterFilms.bind(this);
+    this.sendNewData = this.sendNewData.bind(this);
+    this.sendValueCheckbox = this.sendValueCheckbox.bind(this);
+    this.addFilmToList = this.addFilmToList.bind(this);
   }
 
   filterFilms() {
     let films = this.state.movies;
-      if (this.state.searchText !== "" ){
-      films = films.filter(film =>
+      if (this.state.searchText !== '') {
+        films = films.filter(film =>
         film.title.includes(this.state.searchText) ||
         film.subtitle.includes(this.state.searchText) ||
-        film.storyline.includes(this.state.searchText)
-        );
-      }
-      if(this.state.bookmarkedOnly){
-        films = films.filter(film => film.bookmarked)
-        console.log(films)
-      }
-      if(this.state.selectedGenre.length > 0){
-        films = films.filter(film => film.genre === this.state.selectedGenre)
-      }
+        film.storyline.includes(this.state.searchText),
+      );
+    }
+
+    if (this.state.bookmarkedOnly) {
+      films = films.filter(film => film.bookmarked);
+    }
+
+    if (this.state.selectedGenre.length > 0) {
+      films = films.filter(film => film.genre === this.state.selectedGenre)
+    }
+
     return films;
   }
 
@@ -49,10 +51,10 @@ class MovieLibrary extends Component {
   }
 
   addFilmToList(movie) {
-    this.setState({ movies: [...this.state.movies, movie]});
+    this.setState({ movies: [...this.state.movies, movie] });
   }
 
-  
+
   render() {
     return (
       <div>
@@ -66,7 +68,7 @@ class MovieLibrary extends Component {
           onSelectedGenreChange={(e) => this.sendNewData(e, 'selectedGenre')}
         />
         <MovieList movies={this.filterFilms()} />
-        <AddMovie onClick={this.addFilmToList}/>
+        <AddMovie onClick={this.addFilmToList} />
       </div>
     );
   }
