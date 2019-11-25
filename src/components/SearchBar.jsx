ee3b78e23d234dbeb8f836f63862445812344741
Anderson Bolivar <bolivar.anderson@hotmail.com>
2019-11-25 12:28:16 -0200

    Code Climate Refator

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,23 +3,19 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   render() {
     const genreOptions = [
-      { text: 'Todos', value: '' },
-      { text: 'Ação', value: 'action' },
-      { text: 'Comédia', value: 'comedy' },
-      { text: 'Suspense', value: 'thriller' },
-    ];
+      { text: 'Todos', value: '' }, { text: 'Ação', value: 'action' },
+      { text: 'Comédia', value: 'comedy' }, { text: 'Suspense', value: 'thriller' }];
     return (
       <form>
-        <label htmlFor="search-bar">
-          Inclui o texto:
-        <input type="text" name="search-bar" id="search-bar" value={this.props.searchText} onChange={this.props.onSearchTextChange} />
+        <label htmlFor="search-bar">Inclui o texto:
+        <input type="text" name="search-bar" id="search-bar"
+            value={this.props.searchText} onChange={this.props.onSearchTextChange} />
         </label>
-        <label htmlFor="checkbox">
-          Mostrar somente favoritos:
-          <input type="checkbox" name="checkbox" id="checkbox" checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange} />
+        <label htmlFor="checkbox">Mostrar somente favoritos:
+          <input type="checkbox" name="checkbox" id="checkbox"
+            checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange} />
         </label>
-        <label htmlFor="select">
-          Filtrar por gênero:
+        <label htmlFor="select">Filtrar por gênero:
           <select id="select" value={this.props.selectedGenre}
             onChange={this.props.onSelectedGenreChange}>
             {genreOptions.map((option) =>
