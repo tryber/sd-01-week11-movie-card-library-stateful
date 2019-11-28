19b833ca5e373bef30c0c701664755b3d1fbf372
Conrado Medeiros <conradodemedeiros@hotmail.com>
2019-11-28 12:41:19 -0300

    code climate

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,18 +1,38 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { value: '' };
-
-  //   this.handleChange = this.handleChange.bind(this);
-  //   this.handleSubmit = this.handleSubmit.bind(this);
-  // }
-
   handleChange(event) {
     this.setState({ value: event.target.value });
   }
-
+  createInputSearch(value, func) {
+    return (
+      <label>
+        Inclui o texto:
+        <input type="text" value={value} onChange={func} />
+      </label>
+    );
+  }
+  createSelectBook(value, func) {
+    return (
+      <label>
+        Mostrar somente favoritos
+        <input name="isGoing" type="checkbox" checked={value} onChange={func} />
+      </label>
+    );
+  }
+  createSelectedGenre(value, func) {
+    return (
+      <label>
+        Filtrar por gênero
+        <select value={value} onChange={func}>
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
 
   render() {
     const {
@@ -25,29 +45,11 @@ class SearchBar extends React.Component {
     } = this.props;
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
-          Inclui o texto:
-          <input type='text' value={searchText} onChange={onSearchTextChange} />
-        </label>
+        {this.createInputSearch(searchText, onSearchTextChange)}
         <br />
-        <label>
-        Mostrar somente favoritos
-          <input
-            name='isGoing'
-            type='checkbox'
-            checked={bookmarkedOnly}
-            onChange={onBookmarkedChange} />
-        </label>
+        {this.createSelectBook(bookmarkedOnly, onBookmarkedChange)}
         <br />
-        <label>
-          Filtrar por gênero
-          <select value={selectedGenre} onChange={onSelectedGenreChange}>
-            <option value=''>Todos</option>
-            <option value='action'>Ação</option>
-            <option value='comedy'>Comédia</option>
-            <option value='thriller'>Suspense</option>
-          </select>
-        </label>
+        {this.createSelectedGenre(selectedGenre, onSelectedGenreChange)}
       </form>
     );
   }
